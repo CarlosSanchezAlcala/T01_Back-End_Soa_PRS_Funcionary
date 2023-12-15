647e8cf18a5d84e9b690696642fd4483d060742b
Carlos Edidson SANCHEZ ALCALA <89946636+CarlosSanchezAlcala@users.noreply.github.com>
2023-12-14 23:41:41 -0500

    Delete src directory

--- src/main/java/com/soa/canete/funcionary_soa_canete/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.soa.canete.funcionary_soa_canete.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    private static final String DESCRIPTION = "Resource Not Found Exception";
-
-    public ResourceNotFoundException(String detail) {
-        super(String.format("%s - %s", DESCRIPTION, detail));
-    }
-}
