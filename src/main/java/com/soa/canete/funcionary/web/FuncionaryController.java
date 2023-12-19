3e8d09006307b9ac32cae177f66ecf27372a5efd
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-12-18 21:50:33 -0500

    [UPDATE] Funcionary Securization

--- src/main/java/com/soa/canete/funcionary/web/FuncionaryController.java
+++ src/main/java/com/soa/canete/funcionary/web/FuncionaryController.java
@@ -14,7 +14,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/funcionaryData")
 @RequiredArgsConstructor
 public class FuncionaryController {
